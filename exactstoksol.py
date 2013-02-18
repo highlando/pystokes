4321b4a940ccb803755613d2d484e974f67a40f6
highlando <jnhlnd@gmail.com>
2013-02-18 21:17:56 +0100

    advection to exasol

--- exactstoksol.py
+++ exactstoksol.py
@@ -8,12 +8,16 @@ u1 = ft*x*x*(1-x)*(1-x)*2*y*(1-y)*(2*y-1)
 u2 = ft*y*y*(1-y)*(1-y)*2*x*(1-x)*(1-2*x)
 p = ft*x*(1-x)*y*(1-y)
 
-#Stokes case
-rhs1 = diff(u1,t)-nu*( diff(u1,x,x)+diff(u1,y,y) )+diff(p,x)
-rhs2 = diff(u2,t)-nu*( diff(u2,x,x)+diff(u2,y,y) )+diff(p,y)
+# Stokes case
+rhs1 = diff(u1,t) - nu*(diff(u1,x,x) + diff(u1,y,y)) + diff(p,x)
+rhs2 = diff(u2,t) - nu*(diff(u2,x,x) + diff(u2,y,y)) + diff(p,y)
 
 #rhs3 = div u --- should be zero!!
-rhs3 = diff(u1,x)+diff(u2,y)
+rhs3 = diff(u1,x) + diff(u2,y)
+
+# Advection (u.D)u
+ad1 = u1*diff(u1,x) + u2*diff(u1,y)
+ad2 = u1*diff(u2,x) + u2*diff(u2,y)
 
 rhs1 = smp.simplify(rhs1)
 rhs2 = smp.simplify(rhs2)
