f66258b125cf6e1025cf798127c8128723f8124e
highlando <jnhlnd@gmail.com>
2013-03-07 10:32:40 +0100

    utest for nse solution started

--- utests.py
+++ utests.py
@@ -22,7 +22,38 @@ class TestSequenceFunctions(unittest.TestCase):
 		self.assertTrue(np.allclose(MatRa*vra,mat*va))
 
 	def test_nse_solution(self):
+		from dolfin import *
 		import test_time_schemes as tts
+
+		PrP = ProbParams(N)
+		# get system matrices as np.arrays
+		Ma, Aa, BTa, Ba = dtn.get_sysNSmats(PrP.V, PrP.Q)
+
+		PrP.fv.t  = tcur
+		PrP.fv.nu = nu   # nu = 0, for Eulerian Flow - no diffusion
+
+		fv, fp = dtn.setget_rhs(PrP.V, PrP.Q, PrP.fv, PrP.fp)
+
+		Mc, Ac, BTc, Bc, fvbc, fpbc, bcinds, bcvals, invinds = \
+				dtn.condense_sysmatsbybcs(Ma,Aa,BTa,Ba,fv,fp,PrP.velbcs)
+
+		v, p = PrP.v, PrP.p
+
+		def get_funcexpr_as_condensedvec(u,U,invinds,t=None):
+			if t is not None:
+				u.t = t
+			ua = project(u,U)
+			ua = ua.vector().array()
+			uc = np.atleast_2d(ua[invinds].T)
+			return uc
+
+		vcc = get_funcexpr_as_condensedvec(vCur, PrP.V, invinds, t=tcur)
+		pcc = get_funcexpr_as_condensedvec(pCur, PrP.Q,
+				range(PrP.Q.dim()-1), t=tcur)
+		vnc = get_funcexpr_as_condensedvec(vNex, PrP.V, invinds, t=tcur+dt)
+		ConV  = dtn.get_convvec(v, PrP.V)
+
+		resVc = Mc*(vnc - vcc) + dt*(nu*Ac*vcc 
 
 
 if __name__ == '__main__':
