c0a993cb7d51f221d48be8915cc17fd41c8f31bf
highlando <jnhlnd@gmail.com>
2013-04-05 18:34:51 +0200

    plot_utils +

---
+++ plot_utils.py
@@ -0,0 +1,91 @@
+import json
+import matplotlib.pyplot as plt
+
+def save_simu(TsP, PrP):
+
+	DictOfVals = {'SpaceDiscParam': PrP.N,
+			'Omega': PrP.omega,
+			'TimeInterval':[TsP.t0,TsP.tE],
+			'TimeDiscs': TsP.Ntslist,
+			'LinaTol': TsP.linatol,
+			'TimeIntMeth': TsP.method,
+			'Split': TsP.Split,
+			'ContiRes': TsP.Residuals.ContiRes,
+			'VelEr': TsP.Residuals.VelEr,
+			'PEr': TsP.Residuals.PEr}
+
+	JsFile = 'json/Omeg%dTol%0.0eNTs%dto%dMesh%d' % (DictOfVals['Omega'], TsP.linatol, TsP.Ntslist[0], TsP.Ntslist[-1], PrP.N) +TsP.method + '.json'
+
+	f = open(JsFile, 'w')
+	f.write(json.dumps(DictOfVals))
+
+	print 'For the error plot, run \nplu.jsd_plot_errs("' + JsFile + '")'
+
+	return
+
+def load_json_dicts(StrToJs):
+
+	fjs = open(StrToJs)
+	JsDict = json.load(fjs)
+	return JsDict
+
+
+def merge_json_dicts(CurDi,DiToAppend):
+
+	Jsc = load_json_dicts(CurDi)
+	Jsa = load_json_dicts(DiToAppend)
+
+	if Jsc['SpaceDiscParam'] != Jsa['SpaceDiscParam'] or Jsc['Omega'] != Jsa['Omega']:
+		raise Warning('Space discretization or omega do not match')
+
+	Jsc['TimeDiscs'].extend(Jsa['TimeDiscs'])
+	Jsc['ContiRes'].extend(Jsa['ContiRes'])
+	Jsc['VelEr'].extend(Jsa['VelEr'])
+	Jsc['PEr'].extend(Jsa['PEr'])
+
+	JsFile = 'json/MrgdOmeg%dTol%0.0eNTs%dto%dMesh%d' % (Jsc['Omega'], Jsc['LinaTol'], Jsc['TimeDiscs'][0], Jsc['TimeDiscs'][-1], Jsc['SpaceDiscParam']) +Jsc['TimeIntMeth'] + '.json'
+
+	f = open(JsFile, 'w')
+	f.write(json.dumps(Jsc))
+
+	print 'Merged data stored in \n("' + JsFile + '")'
+
+	return 
+
+def convpltjsd(Jsc):
+
+	Jsc = load_json_dicts(Jsc)
+	
+	Mdict = {'HalfExpEulInd2': 'Ind2', 'HalfExpEulSmaMin': 'Ind1'}
+	JsFile = 'om%d' % Jsc['Omega'] + 'json/' + Mdict[Jsc['TimeIntMeth']] + 'Tol%0.0eN%d' % (Jsc['LinaTol'], Jsc['SpaceDiscParam']) + '.json'
+
+	f = open(JsFile, 'w')
+	f.write(json.dumps(Jsc))
+
+	print '"Data stored in ' + JsFile + '"'
+
+	return
+	
+def jsd_plot_errs(JsDict):
+
+	JsDict = load_json_dicts(JsDict)
+
+	plt.close('all')
+	for i in range(len(JsDict['TimeDiscs'])):
+		leg = 'NTs = $%d$' % JsDict['TimeDiscs'][i]
+		plt.figure(1)
+		plt.plot(JsDict['ContiRes'][i],label=leg)
+		plt.title(JsDict['TimeIntMeth']+': continuity eqn residual')
+		plt.legend()
+		plt.figure(2)
+		plt.plot(JsDict['VelEr'][i],label=leg)
+		plt.title(JsDict['TimeIntMeth']+': Velocity error')
+		plt.legend()
+		plt.figure(3)
+		plt.plot(JsDict['PEr'][i],label=leg)
+		plt.title(JsDict['TimeIntMeth']+': Pressure error')
+		plt.legend()
+
+	plt.show(block=False)
+
+	return
