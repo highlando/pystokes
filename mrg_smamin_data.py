a18d1240c0525fe831df3fdb877a268320ca2a72
highlando <jnhlnd@gmail.com>
2013-04-08 09:02:48 +0200

    data+

--- mrg_smamin_data.py
+++ mrg_smamin_data.py
@@ -1,4 +1,4 @@
-tol = '03'
+tol = '04'
 
 plu.merge_json_dicts(    'json/Omeg8Tol1e-' + tol + 'NTs16to16Mesh40HalfExpEulSmaMin.json',
 					     'json/Omeg8Tol1e-' + tol + 'NTs32to32Mesh40HalfExpEulSmaMin.json')
@@ -6,16 +6,16 @@ plu.merge_json_dicts(    'json/Omeg8Tol1e-' + tol + 'NTs16to16Mesh40HalfExpEulSm
 plu.merge_json_dicts(    'json/Omeg8Tol1e-' + tol + 'NTs64to64Mesh40HalfExpEulSmaMin.json',
 					     'json/Omeg8Tol1e-' + tol + 'NTs128to128Mesh40HalfExpEulSmaMin.json')
 
-plu.merge_json_dicts(    'json/Omeg8Tol1e-' + tol + 'NTs256to256Mesh40HalfExpEulSmaMin.json',
-					     'json/Omeg8Tol1e-' + tol + 'NTs512to512Mesh40HalfExpEulSmaMin.json')
+#plu.merge_json_dicts(    'json/Omeg8Tol1e-' + tol + 'NTs256to256Mesh40HalfExpEulSmaMin.json',
+#					     'json/Omeg8Tol1e-' + tol + 'NTs512to512Mesh40HalfExpEulSmaMin.json')
 
 plu.merge_json_dicts('json/MrgdOmeg8Tol1e-' + tol + 'NTs16to32Mesh40HalfExpEulSmaMin.json',
 					 'json/MrgdOmeg8Tol1e-' + tol + 'NTs64to128Mesh40HalfExpEulSmaMin.json')
 
-plu.merge_json_dicts('json/MrgdOmeg8Tol1e-' + tol + 'NTs16to128Mesh40HalfExpEulSmaMin.json',
-					 'json/MrgdOmeg8Tol1e-' + tol + 'NTs256to512Mesh40HalfExpEulSmaMin.json')
+#plu.merge_json_dicts('json/MrgdOmeg8Tol1e-' + tol + 'NTs16to128Mesh40HalfExpEulSmaMin.json',
+#					 'json/MrgdOmeg8Tol1e-' + tol + 'NTs256to512Mesh40HalfExpEulSmaMin.json')
 
-plu.convpltjsd('json/MrgdOmeg8Tol1e-' + tol + 'NTs16to512Mesh40HalfExpEulSmaMin.json')
+plu.convpltjsd('json/MrgdOmeg8Tol1e-' + tol + 'NTs16to128Mesh40HalfExpEulSmaMin.json')
 
 
 
